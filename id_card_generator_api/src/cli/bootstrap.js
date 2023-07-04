8f242898851ab3fd8fdc0a8e59a9a661aa85917f
Amogh Madan <amoghmadaan@gmail.com>
2023-07-04 18:36:05 +0530

    Serparate the logic for request listener.

--- id_card_generator_api/src/cli/bootstrap.js
+++ id_card_generator_api/src/cli/bootstrap.js
@@ -10,25 +10,35 @@ import urlpatterns from '../routes';
 import {MONGODB_URI} from '../settings';
 
 /**
+ * Get request listener.
+ * @return {express.Application} Returns a promise.
+ */
+export function getRequestListener() {
+  const application = express();
+  application.use(helmet());
+  application.use(express.urlencoded({extended: true}));
+  application.use(express.json());
+  application.use(morgan('combined'));
+  application.use(authenticate);
+
+  urlpatterns.forEach((router, pattern) => {
+    application.use(pattern, router);
+  });
+
+  return application;
+}
+
+/**
  * Bootstrap applicaiton.
  * @param {Number} port, Accepts a port
  * @param {String} host, Accepts a host
  * @return {Promise<void>} Returns a promise.
  */
 export async function bootstrap(port, host) {
-  const app = express();
-  app.use(helmet());
-  app.use(express.urlencoded({extended: true}));
-  app.use(express.json());
-  app.use(morgan('combined'));
-  app.use(authenticate);
+  const requestListener = getRequestListener();
 
-  urlpatterns.forEach((router, pattern) => {
-    app.use(pattern, router);
-  });
-
-  const serverOptions = {};
-  const server = new Server(serverOptions, app);
+  const options = {};
+  const server = new Server(options, requestListener);
   await mongoose.connect(MONGODB_URI);
   server.listen(port, host, () => {
     console.log(server.address());
